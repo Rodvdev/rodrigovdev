9d36b722fddac8badbd600ecb23bebe6a357c6ba
Rodrigo Vásquez de Velasco Gonzales Vigil <87100608+rodrigovdv01@users.noreply.github.com>
2023-08-07 03:19:42 -0500

    a

--- src/App.js
+++ src/App.js
@@ -13,12 +13,5 @@ function App() {
 
 document.body.style.overflowY = "hidden";
 
-// Cambiar el título y la descripción de la página
-document.title = "Desarrollador Web Freelance - Creación y Diseño de Páginas Web";
-document.querySelector('meta[name="description"]').setAttribute(
-  "content",
-  "Desarrollador web freelance especializado en la creación y diseño de páginas web. Ofrezco soluciones personalizadas para tu presencia en línea. Contáctame para proyectos de desarrollo y diseño web."
-);
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -12,7 +12,12 @@ root.render(
   </React.StrictMode>
 );
 
-
+// Cambiar el título y la descripción de la página
+document.title = "Desarrollador Web Freelance - Creación y Diseño de Páginas Web";
+document.querySelector('meta[name="description"]').setAttribute(
+  "content",
+  "Desarrollador web freelance especializado en la creación y diseño de páginas web. Ofrezco soluciones personalizadas para tu presencia en línea. Contáctame para proyectos de desarrollo y diseño web."
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
