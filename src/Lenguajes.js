5e3ac50a8ab39e9846fc071f55475109e858a5a7
Rodrigo Vásquez de Velasco Gonzales Vigil <87100608+rodrigovdv01@users.noreply.github.com>
2023-08-08 02:26:37 -0500

    actualización

--- src/Lenguajes.js
+++ src/Lenguajes.js
@@ -4,25 +4,33 @@ import html_logo from "./HTML5_logo.svg";
 import CSS3_logo from "./CSS3_logo.svg";
 import JS_logo from "./JS_logo.svg";
 
+function Logo({ src, className, alt }) {
+  return <img
+    src={src}
+    className={className}
+    alt={alt}
+  />;
+}
+
 export default function Lenguajes() {
   return (
     <div className="lenguajes">
-      <img
+      <Logo
         src={React_logo}
         className="App-logo elemento"
         alt="REACT - Framework  JavaScript front-end"
       />
-      <img
+      <Logo
         src={html_logo}
         className="html-logo elemento"
         alt="HTML5 -Lenguaje de hipermarcado de texto"
       />
-      <img
+      <Logo
         src={CSS3_logo}
         className="css-logo elemento"
         alt="CSS3 -Estilos en cascada"
       />
-      <img
+      <Logo
         src={JS_logo}
         className="js-logo elemento"
         alt="JavaScript - Lenguaje de programación"
