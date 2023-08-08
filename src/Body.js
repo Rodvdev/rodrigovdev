b10dd2ea0d88a45e835306ae23927a6ad4178692
Rodrigo Vásquez de Velasco Gonzales Vigil <87100608+rodrigovdv01@users.noreply.github.com>
2023-08-08 00:42:20 -0500

    act

--- src/Body.js
+++ src/Body.js
@@ -5,7 +5,7 @@ export default function Body() {
   return (
     <div className="body">
       <div className="name-description">
-        <RainbowText text="Hola, soy Rodrigo VdeV" />
+        <RainbowText text="Rodrigo Vásquez de Velasco" />
         <RainbowText text="Experto en Diseño y Desarrollo Frontend" />
       </div>
     </div>
